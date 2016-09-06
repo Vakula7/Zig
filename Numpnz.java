eaf444ea3042c5f9d798e95eae5dc9b0e5a0bdf1
Vakula7 <vakulasona96@gmail.com>
2016-09-06 23:00:25 +0530

    Create Numpnz.java

---
+++ Numpnz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Numpnz {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		if(num>0)
+		System.out.println("Postive");
+		else if(num<0)
+		System.out.println("negative");
+		else
+		System.out.println("zero");
+		
+
+	}
+
+}
